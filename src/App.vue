04a155fca18d34866977e471ce2dcdb2fa02257b
yuduot03qoo <yuduot03@gmail.com>
2025-04-03 18:45:13 +0800

    123132

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="app">
     <a class="box1" target="_blank" href="https://pay.maple-moon.tw/order/249t38q"><img src="./assets/sponser1.png" /></a>
+    <a class="box4" target="_blank" href="https://pay.maple-moon.tw/order/%E4%BA%8C%E6%9C%8D"><img src="./assets/sponser2.png" /></a>
     <div class="box2"><img src="./assets/cscontact.png" /></div>
     <div class="box3"><img src="./assets/sponser3.png" /></div>
+    <img src="./assets/logo1.png" class="hide" />
     <Header></Header>
     <router-view></router-view>
   </div>
@@ -15,14 +17,14 @@ import Header from "./components/Header.vue";
 .box1 {
   display: block;
   position: fixed;
-  top: 50%;
+  top: 20%;
   left: 0.5%;
   z-index: 9;
 }
 
 .box2 {
   position: fixed;
-  top: 35%;
+  top: 50%;
   right: 0.5%;
   z-index: 9;
 }
@@ -31,7 +33,17 @@ import Header from "./components/Header.vue";
   display: block;
   position: fixed;
   top: 20%;
+  right: 0.5%;
+  z-index: 9;
+}
+.box4 {
+  display: block;
+  position: fixed;
+  top: 50%;
   left: 0.5%;
   z-index: 9;
+}
+.hide {
+  display: none;
 }
 </style>
